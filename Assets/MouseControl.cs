19f27042f45ff5da2f5a8005d653b7ae3dcfa017
king <KingFxChannel@gmail.com>
2015-11-23 19:44:47 -0600

    Modified the Level Editor

---
+++ Assets/MouseControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit, 1000)) {
+        //    hit.transform.GetComponent<SpriteRenderer>().color = Color.white;
+        //}
+    }
+}
---
+++ Assets/SlotScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlotScript : MonoBehaviour {
+
+    private Color black;
+    private Color32 white;
+
+	// Use this for initialization
+	void Start () {
+        black = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = black;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseEnter () {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    void OnMouseExit() {
+        GetComponent<SpriteRenderer>().color = black;
+    }
+}
